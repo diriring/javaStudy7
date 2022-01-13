1ec76b9d1766f2e53cb9cefb6454acd1e3d28e5a
jy_lee <qjarlfls@naver.com>
2022-01-13 16:33:26 +0900

    add DTO

---
+++ src/com/java/s1/adressbook/PeopleDTO.java
@@ -0,0 +1,41 @@
+package com.java.s1.adressbook;
+
+public class PeopleDTO {
+	
+	private String name;
+	private String nickname;
+	private String email;
+	private String phonenum;
+	private String birth;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhonenum() {
+		return phonenum;
+	}
+	public void setPhonenum(String phonenum) {
+		this.phonenum = phonenum;
+	}
+	public String getBirth() {
+		return birth;
+	}
+	public void setBirth(String birth) {
+		this.birth = birth;
+	}
+
+}
